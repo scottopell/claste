01e873a48227d8a598766fec4ca56b285091930b
Scott Opell <me@scottopell.com>
2024-04-13 16:31:37 -0400

    Initial commit

---
+++ claste/ClipboardManager.swift
@@ -0,0 +1,8 @@
+//
+//  ClipboardManager.swift
+//  claste
+//
+//  Created by Scott Opell on 4/13/24.
+//
+
+import Foundation
---
+++ claste/ExpandableTextView.swift
@@ -0,0 +1,8 @@
+//
+//  ExpandableTextView.swift
+//  claste
+//
+//  Created by Scott Opell on 4/13/24.
+//
+
+import Foundation
