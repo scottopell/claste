c1e7f34b76de638caf1a47a66f6b3eb3751db603
Scott Opell <me@scottopell.com>
2024-04-13 09:43:49 -0400

    Initial Commit

---
+++ claste/clasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  clasteApp.swift
+//  claste
+//
+//  Created by Scott Opell on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct clasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
